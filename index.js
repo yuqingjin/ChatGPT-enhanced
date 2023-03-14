b31c7c97d87585e9cdec43a4323e42489ac4279d
Yuqing Jin <yj2679@columbia.edu>
2023-03-14 19:34:00 -0400

    create adjusted chatgpt

---
+++ index.js
@@ -0,0 +1,55 @@
+// openAI api key
+// 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
+
+const { Configuration, OpenAIApi } = require("openai");
+const express = require("express")
+const configuration = new Configuration({
+    organization: "org-Knw8izOVnXv3GlButBqJ5VWs",
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+// create a simple express api that calls the function above
+const app = express();
+const cors = require("cors")
+const bodyParser = require("body-parser")
+app.use(bodyParser.json())
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+const port = 3001;
+
+app.post("/", async(req, res) => {
+    const { message, currentModel } = req.body;
+    console.log("received message: ", message)
+    console.log("current model: ", currentModel)
+
+    const response = await openai.createCompletion({
+        model: currentModel === 'default'? "text-davinci-003" : currentModel,
+        prompt:`${message}`,
+        max_tokens: 100,
+        temperature: 0.5,
+    });
+    if (response.data.choices[0].text) {
+        res.json({
+            message: response.data.choices[0].text
+        })
+    }
+    else {
+        res.json({
+            message: response.data.error.message
+        })
+    }
+
+})
+
+app.get("/models", async(req, res) => {
+    const response = await openai.listEngines();
+    // console.log("response", response.data.data)
+    res.json({
+        models: response.data.data
+    })
+})
+
+app.listen(port, ()=> {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
